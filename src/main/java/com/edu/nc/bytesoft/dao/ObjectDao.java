0eec95d5c0c0f5322797bbfe2ada7b9a22f2784c
zilidev <zilian.nf@gmail.com>
2015-11-23 19:35:40 +0200

    interface change

--- src/main/java/com/edu/nc/bytesoft/dao/ObjectDao.java
+++ src/main/java/com/edu/nc/bytesoft/dao/ObjectDao.java
@@ -1,11 +1,10 @@
 package com.edu.nc.bytesoft.dao;
 
 import com.edu.nc.bytesoft.dao.exception.NoSuchObjectException;
-import com.edu.nc.bytesoft.model.BaseEntity;
 
 import java.sql.SQLException;
 
-public interface ObjectDao<T extends BaseEntity> extends SettableConnection {
+public interface ObjectDao<T> extends SettableConnection {
     T getObjectById(long id) throws SQLException, NoSuchObjectException;
     boolean updateObject(T object) throws NoSuchObjectException;
     boolean deleteObject(T object) throws NoSuchObjectException;
